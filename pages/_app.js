b255c39fdee8a7a6eeecf2f0dc98e24683eb3914
Mansur <funisinfinity26@gmail.com>
2022-08-22 19:16:49 +0600

    add google analytics

--- pages/_app.js
+++ pages/_app.js
@@ -4,7 +4,32 @@ import Footer from '../components/Footer';
 import Header from '../components/Header';
 import '../styles/globals.css';
 
+import { useEffect } from 'react'
+import { useRouter } from 'next/router'
+
+// google analytics
+import ReactGA from 'react-ga4';
+const TRACKING_ID = "G-E0JLTSVL8T";
+ReactGA.initialize(TRACKING_ID);
+
 function MyApp({ Component, pageProps }) {
+    const router = useRouter()
+
+    useEffect(() => {
+        const handleRouteChange = (url) => {
+            // Send pageview with a custom path
+            ReactGA.send({ hitType: "pageview", page: "/my-path" });
+        }
+        router.events.on('routeChangeComplete', handleRouteChange)
+
+        // If the component is unmounted, unsubscribe
+        // from the event with the `off` method:
+        return () => {
+            router.events.off('routeChangeComplete', handleRouteChange)
+        }
+    }, []);
+
+
     return (
         <>
             <Header />
