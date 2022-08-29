2e878924bc25f8694431ebc9444214bfa729b6bb
Mansur <funisinfinity26@gmail.com>
2022-08-29 16:46:12 +0200

    add new component

---
+++ components/ChipLink.js
@@ -0,0 +1,23 @@
+import Link from './Link';
+import { Chip } from '@mui/material';
+
+function ChipLink({ label, link, icon, ...props }) {
+    const renderedIcon = icon({
+        style: {
+            marginLeft: '0.5em',
+        }
+    })
+
+    return (
+        <Link href={link} underline={'none'}>
+            <Chip
+                icon={renderedIcon}
+                label={label}
+                variant='outlined'
+                sx={{ cursor: 'pointer', ...props.sx }}
+            />
+        </Link>
+    );
+}
+
+export default ChipLink;
