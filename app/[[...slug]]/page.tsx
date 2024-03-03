fa1988b15d19fa98b29d85e31e5af24e47466c38
nomomon <funisinfinity26@gmail.com>
2024-03-03 21:41:01 +0100

    Refactor page components and update file structure

---
+++ app/[[...slug]]/page.tsx
@@ -0,0 +1,47 @@
+import { create, parseMarkdown, walk } from "@/lib/utils";
+import { readFileSync, existsSync } from "fs";
+import { notFound } from "next/navigation";
+import { Metadata, ResolvingMetadata } from "next";
+import { AboutMe } from "@/components/templates/about-me";
+
+const Page = async ({ params }: { params: { slug: string[] } }) => {
+  const { data, markdown, error } = await create(params);
+
+  if (error || !data || !markdown) {
+    return notFound();
+  }
+
+  return (
+    <>
+      {!params.slug && (
+        <div className="mb-8">
+          <AboutMe />
+        </div>
+      )}
+      <div>
+        <div className="markdown-body">
+          <article dangerouslySetInnerHTML={{ __html: markdown }} />
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Page;
+
+export async function generateStaticParams() {
+  const slugs = [];
+
+  for await (const file of walk("public/")) {
+    if (file.endsWith(".md")) {
+      const slug = file.replace("public/", "").replace(".md", "").split("/");
+      slugs.push({ slug });
+
+      if (slug[slug.length - 1] === "index") {
+        slugs.push({ slug: slug.slice(0, -1) });
+      }
+    }
+  }
+
+  return slugs;
+}
